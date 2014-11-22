22ef75aa7716a400f9ebd483574570bdfc2dae8d
Carlos Martín <inean.es@gmail.com>
2014-11-22 21:03:03 +0100

    Set new defaults for pool size, listening address, and db fallback

--- resources/connection.rb
+++ resources/connection.rb
@@ -25,24 +25,24 @@ attribute :db_host, :kind_of => String, :required => true
 attribute :db_port, :kind_of => String, :required => true
 attribute :db_name, :kind_of => String
 
-attribute :use_db_fallback, :kind_of => [TrueClass, FalseClass], :default => false
+attribute :use_db_fallback, :kind_of => [TrueClass, FalseClass], :default => true
 
 attribute :userlist, :kind_of => Hash, :required => true
 attribute :admin_users, :kind_of => Array, :default => []
 attribute :stats_users, :kind_of => Array, :default => []
 
-attribute :listen_addr, :kind_of => String
+attribute :listen_addr, :kind_of => String, :default => '*'
 attribute :listen_port, :kind_of => String
 
 attribute :user, :kind_of => String, :default => 'pgbouncer'
 attribute :group, :kind_of => String, :default => 'pgbouncer'
 attribute :log_dir, :kind_of => String, :default => '/var/log/pgbouncer'
-attribute :socket_dir, :kind_of => String, :default => '/etc/pgbouncer/db_sockets'
+attribute :socket_dir, :kind_of => String, :default => '/var/run/pgbouncer'
 attribute :pid_dir, :kind_of => String, :default => '/var/run/pgbouncer'
 
 attribute :pool_mode, :kind_of => String, :default => 'transaction'
-attribute :max_client_conn, :kind_of => Integer, :default => 60
-attribute :default_pool_size, :kind_of => Integer, :default => 30
+attribute :max_client_conn, :kind_of => Integer, :default => 1000
+attribute :default_pool_size, :kind_of => Integer, :default => 50
 attribute :min_pool_size, :kind_of => Integer, :default => 10
 attribute :reserve_pool_size, :kind_of => Integer, :default => 5
 attribute :server_idle_timeout, :kind_of => Integer, :default => 3600
@@ -52,3 +52,13 @@ attribute :connect_query, :kind_of => String
 attribute :tcp_keepalive, :kind_of => Integer
 attribute :tcp_keepidle, :kind_of => Integer
 attribute :tcp_keepintvl, :kind_of => Integer
+
+attribute :server_check_query, :kind_of => String
+attribute :log_connections, :kind_of => Integer
+attribute :log_disconnections, :kind_of => Integer
+attribute :log_pooler_errors, :kind_of => Integer
+attribute :server_lifetime, :kind_of => Integer
+
+attribute :ignore_startup_parameters, :kind_of => String
+attribute :server_check_delay, :kind_of => Integer
+attribute :reserve_pool_timeout, :kind_of => Integer
