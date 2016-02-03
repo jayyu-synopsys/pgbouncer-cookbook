9b16888de7ecf6cfd485c1471125a8c28040a79e
Vladimir Kamarzin <vvk@vvk.pp.ru>
2016-02-03 14:50:13 +0500

    Disable default system pgboucner service on ubuntu

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,3 +16,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# disable default system pgbouncer service
+if node['platform'] == 'ubuntu'
+    cookbook_file "/etc/default/pgbouncer" do
+        backup false
+        owner "root"
+        group "root"
+        mode 0644
+    end
+end
